9f704f2ad714e2bdfa217a19fe75d77bf5a751b8
michkinn <michkinn@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-12-18 07:53:21 -0600

    git-svn-id: http://svn.symfony-project.com/plugins/sfJqueryTreeDoctrineManagerPlugin/trunk@25586 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfJqueryTreeDoctrineManager/templates/_manager.php
+++ modules/sfJqueryTreeDoctrineManager/templates/_manager.php
@@ -1,20 +1,20 @@
 <?php use_helper('I18N', 'Date', 'JavascriptBase') ?>
-<?php include_partial('sfJsTreeDoctrine/assets') ?>
+<?php include_partial('sfJqueryTreeDoctrine/assets') ?>
 <div id="sf_admin_container">
     <h1><?php echo sfInflector::humanize(sfInflector::underscore($model)); ?> Nested Set Manager</h1>
-    <?php include_partial('sfJsTreeDoctrine/flashes') ?>
+    <?php include_partial('sfJqueryTreeDoctrine/flashes') ?>
     
     
     <div class="nested_set_manager_holder" id="<?php echo strtolower($model); ?>_nested_set_manager_holder">
         <?php //echo '<pre>' . print_r($records,1) . '</pre>';?>
-        <?php echo get_partial('sfJsTreeDoctrine/nested_set_list', array('model' => $model, 'field' => $field, 'root' => $root, 'records' => $records)); ?>
+        <?php echo get_partial('sfJqueryTreeDoctrine/nested_set_list', array('model' => $model, 'field' => $field, 'root' => $root, 'records' => $records)); ?>
         <div style="clear:both"></div>
     </div>
     
     
     <ul class="sf_admin_actions">
-      <?php include_partial('sfJsTreeDoctrine/list_batch_actions') ?>
-      <?php include_partial('sfJsTreeDoctrine/list_actions', array('model' => $model,'field' => $field)) ?>
+      <?php include_partial('sfJqueryTreeDoctrine/list_batch_actions') ?>
+      <?php include_partial('sfJqueryTreeDoctrine/list_actions', array('model' => $model,'field' => $field)) ?>
     </ul>
     
 </div>
