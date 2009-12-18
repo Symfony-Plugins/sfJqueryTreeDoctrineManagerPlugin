50e0dcaed76d29aee393dfbca6dc1f7ba31103f6
michkinn <michkinn@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-12-18 07:46:56 -0600

    Call the right component
    git-svn-id: http://svn.symfony-project.com/plugins/sfJqueryTreeDoctrineManagerPlugin/trunk@25583 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/sfJqueryTreeDoctrineHelper.php
+++ lib/helper/sfJqueryTreeDoctrineHelper.php
@@ -2,5 +2,5 @@
     function get_nested_set_manager($model, $field, $root = 0){
         sfContext::getInstance()->getResponse()->addStylesheet('/sfJqueryTreeDoctrinePlugin/css/screen.css');
         sfContext::getInstance()->getResponse()->addJavascript('/sfJqueryTreeDoctrinePlugin/js/jquery.tree.min.js');
-        return get_component('sfJsTreeDoctrine', 'manager', array('model' => $model, 'field' => $field, 'root' => $root));
+        return get_component('sfJqueryTreeDoctrine', 'manager', array('model' => $model, 'field' => $field, 'root' => $root));
     }
