f6668812990399e0f193abb9a68f1deba8c59e95
michkinn <michkinn@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-02-10 16:45:18 -0600

    Remove unused link to cookie.js remove SVN external
    git-svn-id: http://svn.symfony-project.com/plugins/sfJqueryTreeDoctrineManagerPlugin/trunk@27866 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/sfJqueryTreeDoctrineHelper.php
+++ lib/helper/sfJqueryTreeDoctrineHelper.php
@@ -3,7 +3,8 @@
 		function get_nested_set_manager($model, $field, $root = 0){
         sfContext::getInstance()->getResponse()->addStylesheet('/sfJqueryTreeDoctrineManagerPlugin/jsTree/themes/default/style.css');
         sfContext::getInstance()->getResponse()->addStylesheet('/sfJqueryTreeDoctrineManagerPlugin/css/screen.css');
-        sfContext::getInstance()->getResponse()->addJavascript('/sfJqueryTreeDoctrineManagerPlugin/jsTree/lib/jquery.js');
+        sfContext::getInstance()->getResponse()->addJavascript('/sfJqueryTreeDoctrineManagerPlugin/jsTree/jquery.js');
+				sfContext::getInstance()->getResponse()->addJavascript('/sfJqueryTreeDoctrineManagerPlugin/jsTree/jquery.cookie.js');
         sfContext::getInstance()->getResponse()->addJavascript('/sfJqueryTreeDoctrineManagerPlugin/jsTree/jquery.tree.min.js');
 				sfContext::getInstance()->getResponse()->addJavascript('/sfJqueryTreeDoctrineManagerPlugin/jsTree/plugins/jquery.tree.cookie.js');
         return get_component('sfJqueryTreeDoctrineManager', 'manager', array('model' => $model, 'field' => $field, 'root' => $root));
