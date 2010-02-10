a09efc82af2c9a7a10cab333232a7079a1302e27
michkinn <michkinn@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-02-10 16:34:35 -0600

    Remove unused link to cookie.js update SVN external
    git-svn-id: http://svn.symfony-project.com/plugins/sfJqueryTreeDoctrineManagerPlugin/trunk@27864 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/sfJqueryTreeDoctrineHelper.php
+++ lib/helper/sfJqueryTreeDoctrineHelper.php
@@ -4,7 +4,6 @@
         sfContext::getInstance()->getResponse()->addStylesheet('/sfJqueryTreeDoctrineManagerPlugin/jsTree/themes/default/style.css');
         sfContext::getInstance()->getResponse()->addStylesheet('/sfJqueryTreeDoctrineManagerPlugin/css/screen.css');
         sfContext::getInstance()->getResponse()->addJavascript('/sfJqueryTreeDoctrineManagerPlugin/jsTree/lib/jquery.js');
-				sfContext::getInstance()->getResponse()->addJavascript('/sfJqueryTreeDoctrineManagerPlugin/jsTree/lib/jquery.cookie.js');
         sfContext::getInstance()->getResponse()->addJavascript('/sfJqueryTreeDoctrineManagerPlugin/jsTree/jquery.tree.min.js');
 				sfContext::getInstance()->getResponse()->addJavascript('/sfJqueryTreeDoctrineManagerPlugin/jsTree/plugins/jquery.tree.cookie.js');
         return get_component('sfJqueryTreeDoctrineManager', 'manager', array('model' => $model, 'field' => $field, 'root' => $root));
