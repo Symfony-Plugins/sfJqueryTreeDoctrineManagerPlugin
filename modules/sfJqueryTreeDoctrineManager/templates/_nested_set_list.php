2e8f536aeae388c3deac669a2c52acdafb7b0e4e
songecko <songecko@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-08-05 15:23:09 -0500

    [sfJqueryTreeDoctrineManagerPlugin] Fix a bug in the _nested_set_list.php partial.
    git-svn-id: http://svn.symfony-project.com/plugins/sfJqueryTreeDoctrineManagerPlugin/trunk@30546 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfJqueryTreeDoctrineManager/templates/_nested_set_list.php
+++ modules/sfJqueryTreeDoctrineManager/templates/_nested_set_list.php
@@ -1,22 +1,32 @@
  <?php if( isset($records) && is_object($records) && count($records) > 0 ): ?>
     <div id="<?php echo strtolower($model);?>-nested-set">
         <ul class="nested_set_list">
-        <?php $prevLevel = 0;?>      
-        <?php foreach($records as $record): ?>
-            <?php if($prevLevel > 0 && $record['level'] == $prevLevel)  echo '</li>';
-            if($record['level'] > $prevLevel)  echo '<ul>'; 
-            elseif ($record['level'] < $prevLevel) echo str_repeat('</ul></li>', $prevLevel - $record['level']); ?>
+<?php       $prevLevel = 0;?>
+<?php       $levels = 0;?>    
+<?php       foreach($records as $record): ?>
+<?php           if($prevLevel > 0 && $record['level'] == $prevLevel): ?>
+        	</li> 
+<?php           endif; ?>
+<?php           if($record['level'] > $prevLevel): $levels++; ?> 
+		<ul>
+		
+<?php           elseif($record['level'] < $prevLevel): ?>
+            <?php echo  str_repeat('</li></ul>', $prevLevel - $record['level']); ?> 
+<?php           endif; ?>
             <li id ="phtml_<?php echo $record->id ?>">
                 <a href="#"><ins>&nbsp;</ins><?php echo $record->$field;?></a>
-            <?php $prevLevel = $record['level'];
-        endforeach; ?>        
+<?php          $prevLevel = $record['level']; ?>
+<?php      endforeach; ?>        
+<?php for($i=1;$i<$levels;$i++):?>
+		</ul>
+<?php endfor; ?>
         </ul>
     </div>
 <?php endif;?>
-<?php echo javascript_tag();?>
-$(function () {
 
-    
+<script type="text/javascript">
+<!--
+$(function () {
 	$("#<?php echo strtolower($model);?>-nested-set").tree({
   	plugins : { 
 			cookie : { prefix : "<?php echo strtolower($model);?>_jstree_" },
@@ -58,9 +68,9 @@ $(function () {
 			onrename : function (NODE, TREE_OBJ, RB) {
                 $('.error').remove();
                 $('.notice').remove();
-				$('.nested_set_manager_holder').before('<div class="waiting"><?php echo __('Sending data to server.');?></div>');
+				$('.nested_set_manager_holder').before('<div class="waiting"><?php echo __('Sending data to server.');?><\/div>');
                 if (TREE_OBJ.get_text(NODE) == 'New folder'){
-                    $('.nested_set_manager_holder').before('<div class="error">"'+TREE_OBJ.get_text(NODE)+'" <?php echo __('is not a valid name');?></div>');
+                    $('.nested_set_manager_holder').before('<div class="error">"'+TREE_OBJ.get_text(NODE)+'" <?php echo __('is not a valid name');?><\/div>');
                     $.tree.focused().rename();
                 }
                 else {
@@ -74,11 +84,11 @@ $(function () {
                                     $('.waiting').remove();
                             },
                             success : function (data, textStatus) {
-                                $('.nested_set_manager_holder').before('<div class="notice"><?php echo __('The item was created successfully.');?></div>');
+                                $('.nested_set_manager_holder').before('<div class="notice"><?php echo __('The item was created successfully.');?><\/div>');
                                 $(NODE).attr('id','phtml_'+data.id);
                             },
                             error : function (data, textStatus) {
-                                $('.nested_set_manager_holder').before('<div class="error"><?php echo __('Error while creating the item.');?></div>');
+                                $('.nested_set_manager_holder').before('<div class="error"><?php echo __('Error while creating the item.');?><\/div>');
                                 $.tree.rollback(RB);
                             }
                         });
@@ -93,10 +103,10 @@ $(function () {
 								$('.waiting').remove();
 							},
 							success : function (data, textStatus) {
-								$('.nested_set_manager_holder').before('<div class="notice"><?php echo __('The item was renamed successfully.');?></div>');
+								$('.nested_set_manager_holder').before('<div class="notice"><?php echo __('The item was renamed successfully.');?><\/div>');
 							},
 							error : function (data, textStatus) {
-								$('.nested_set_manager_holder').before('<div class="error"><?php echo __('Error while renaming the item.');?></div>');
+								$('.nested_set_manager_holder').before('<div class="error"><?php echo __('Error while renaming the item.');?><\/div>');
 								$.tree.rollback(RB);
 							}
 						});
@@ -113,7 +123,7 @@ $(function () {
 			onmove: function(NODE, REF_NODE, TYPE, TREE_OBJ, RB){
 				$('.error').remove();
 				$('.notice').remove();
-				$('.nested_set_manager_holder').before('<div class="waiting"><?php echo __('Sending data to server.');?></div>');
+				$('.nested_set_manager_holder').before('<div class="waiting"><?php echo __('Sending data to server.');?><\/div>');
 				$.ajax({
 					type: "POST",
 					url : '<?php echo url_for('sfJqueryTreeDoctrineManager/Move');?>',
@@ -123,10 +133,10 @@ $(function () {
 						$('.waiting').remove();
 					},
 					success : function (data, textStatus) {
-						$('.nested_set_manager_holder').before('<div class="notice"><?php echo __('The item was moved successfully.');?></div>');
+						$('.nested_set_manager_holder').before('<div class="notice"><?php echo __('The item was moved successfully.');?><\/div>');
 					},
 					error : function (data, textStatus) {
-						$('.nested_set_manager_holder').before('<div class="error"><?php echo __('Error while moving the item.');?></div>');
+						$('.nested_set_manager_holder').before('<div class="error"><?php echo __('Error while moving the item.');?><\/div>');
 						$.tree.rollback(RB);
 					}
 				});
@@ -135,7 +145,7 @@ $(function () {
 			ondelete: function(NODE, TREE_OBJ, RB){
 				$('.error').remove();
 				$('.notice').remove();
-				$('.nested_set_manager_holder').before('<div class="waiting"><?php echo __('Sending data to server.');?></div>');
+				$('.nested_set_manager_holder').before('<div class="waiting"><?php echo __('Sending data to server.');?><\/div>');
 				$.ajax({
 					type: "POST",
 					url : '<?php echo url_for('sfJqueryTreeDoctrineManager/Delete');?>',
@@ -145,10 +155,10 @@ $(function () {
 						$('.waiting').remove();
 					},
 					success : function (data, textStatus) {
-						$('.nested_set_manager_holder').before('<div class="notice"><?php echo __('The item was deleted successfully.');?></div>');
+						$('.nested_set_manager_holder').before('<div class="notice"><?php echo __('The item was deleted successfully.');?><\/div>');
 					},
 					error : function (data, textStatus) {
-						$('.nested_set_manager_holder').before('<div class="error"><?php echo __('Error while deleting the item.');?></div>');
+						$('.nested_set_manager_holder').before('<div class="error"><?php echo __('Error while deleting the item.');?><\/div>');
 						$.tree.rollback(RB);
 					}
 				});
@@ -156,4 +166,6 @@ $(function () {
 		}
 	});
 });
-<?php echo end_javascript_tag();?>
+
+//-->
+</script>
