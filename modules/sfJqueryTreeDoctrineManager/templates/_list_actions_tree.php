02dd278027b2e8a97b1afb4bec74ecaa361cb9ee
songecko <songecko@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-08-05 15:19:47 -0500

    [sfJqueryTreeDoctrineManagerPlugin] Add a new configuration param to specify the route to "back to root".
    git-svn-id: http://svn.symfony-project.com/plugins/sfJqueryTreeDoctrineManagerPlugin/trunk@30545 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfJqueryTreeDoctrineManager/templates/_list_actions_tree.php
+++ modules/sfJqueryTreeDoctrineManager/templates/_list_actions_tree.php
@@ -1,7 +1,14 @@
 <ul class="sf_admin_actions">
 	<?php if($hasManyRoots):?>
  		<li class="sf_admin_action_list">
-		<?php echo link_to( __('Back to root list') ,$sf_request->getParameter('module') . '/' . $sf_request->getParameter('action') );?>
+ 		<?php 
+	 		if(sfConfig::has('app_sfJqueryTree_route_back')){
+	 			$url = sfConfig::get('app_sfJqueryTree_route_back');
+	 		}else{
+	 			$url = $sf_request->getParameter('module').'/'.$sf_request->getParameter('action');
+	 		} 
+ 		?>
+		<?php echo link_to(__('Back to root list'), $url);?>
 	<?php endif;?>
 	<li class="sf_admin_action_insert_node">
 		<button disabled="disabled" class="nodeinteraction createnode">
